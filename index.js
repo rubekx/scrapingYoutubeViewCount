cee34d408f260c60ef6fbacf4d8f30342f7df5d6
Rubem <rubekx@gmail.com>
2020-06-09 21:24:53 -0300

    added function to remove others info from data

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ let urls = [
     'https://www.youtube.com/watch?v=ZYWuFzs4BPA'
 ]
 
+const removeInfo = (str) => {
+    let view_count = str.replace('visualizações', '');
+    return view_count.replace('.', '');
+}
+
 const getCount = (url) => {
     got(url).then(response => {
             let $ = cheerio.load(response.body);
             if (typeof $('div[class=watch-view-count]')[0] === 'undefined')
                 getCount(url)
             else {
-                let tag = $('div[class=watch-view-count]')[0].children[0].data
-                let total = tag.replace('visualizações', ' ');
-                total = total.replace('.', '');
-                console.log(url + ': ' + total)
+                let tag = removeInfo($('div[class=watch-view-count]')[0].children[0].data)
+                console.log(tag)
             }
 
         })
-        .catch(err => {
-            console.log(err);
+        .catch(error => {
+            console.log(error);
         });
 }
 
